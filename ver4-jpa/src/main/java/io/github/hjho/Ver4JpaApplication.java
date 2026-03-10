a25fe44311427099d0a4639aca3135da5858f9f2
hjho <gjwnsgh131@naver.com>
2026-03-10 20:54:39 +0900

    ver4-jpa commit

---
+++ ver4-jpa/src/main/java/io/github/hjho/Ver4JpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.hjho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ver4JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ver4JpaApplication.class, args);
+	}
+
+}
---
+++ ver4-jpa/src/test/java/io/github/hjho/jpa/Ver4JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hjho.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ver4JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
