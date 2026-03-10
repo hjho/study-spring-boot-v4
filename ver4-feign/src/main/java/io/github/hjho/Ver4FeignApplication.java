2a1220253eefc46c994f8d7f70e120e890b03ed7
hjho <gjwnsgh131@naver.com>
2026-03-10 20:54:32 +0900

    ver4-feign commit

---
+++ ver4-feign/src/main/java/io/github/hjho/Ver4FeignApplication.java
@@ -0,0 +1,13 @@
+package io.github.hjho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ver4FeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ver4FeignApplication.class, args);
+	}
+
+}
---
+++ ver4-feign/src/test/java/io/github/hjho/feign/Ver4FeignApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hjho.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ver4FeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
