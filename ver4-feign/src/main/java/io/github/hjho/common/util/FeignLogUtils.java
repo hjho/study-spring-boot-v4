178c427a58c1f039e799d0bf85eab7e46c763a1d
hjho <gjwnsgh131@naver.com>
2026-03-21 20:17:09 +0900

    불필요 코드 제거.

--- ver4-feign/src/main/java/io/github/hjho/common/util/FeignLogUtils.java
+++ ver4-feign/src/main/java/io/github/hjho/common/util/FeignLogUtils.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
 import feign.Request;
-import feign.Request.HttpMethod;
 import feign.Response;
 import feign.Util;
 
@@ -19,43 +18,6 @@ public class FeignLogUtils {
 		Collection<String> values = request.headers().get(headerName);
 		if (values == null || values.isEmpty()) return null;
 		return values.iterator().next();
-	}
-	
-	
-	/**
-	 * URL의 쿼리 스트링과 Form Body 파라미터를 통합하여 추출
-	 */
-	public static String getRequestData(Request request) {
-		StringBuilder params = new StringBuilder();
-		
-		HttpMethod method = request.httpMethod();
-		
-		if(method.equals(HttpMethod.GET)) {
-			
-		}
-		
-		// 1. Content-Type 확인 후 Form 데이터인 경우 바디 파싱
-		String contentType = getHeader(request, "Content-Type");
-		
-		
-		// 1. URL에서 쿼리 파라미터 추출 (GET, POST 공통)
-		// 예: http://api.com/users?id=123&name=test -> id=123&name=test 추출
-		String url = request.url();
-		if (url.contains("?")) {
-			params.append("[Query] ").append(url.substring(url.indexOf("?") + 1));
-		}
-		
-		
-		
-		if (contentType != null && contentType.contains("application/x-www-form-urlencoded")) {
-			String body = getRequestBody(request);
-			if (!body.isEmpty()) {
-				if (params.length() > 0) params.append(" | ");
-				params.append("[Form] ").append(body);
-			}
-		}
-		
-		return params.toString();
 	}
 	
 	/**
@@ -96,5 +58,10 @@ public class FeignLogUtils {
 		
 		return new String(bodyData, StandardCharsets.UTF_8);
 	}
+	
+	
+	
+	
+	
 	
 }
